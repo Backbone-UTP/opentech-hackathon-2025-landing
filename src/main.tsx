24abe0868061d6d928ba11a9e19204cc3843245b
Djkde01 <sergio.estrella@utp.edu.co>
2025-09-10 22:10:44 -0500

    feat: implement routing with react-router-dom for Code of Conduct and Terms of Event pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.tsx'
+import './index.css'
+import CodeOfConduct from './pages/CodeOfConduct.tsx';
+import TermsOfEvent from './pages/TermsOfEvent.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/code-of-conduct",
+    element: <CodeOfConduct />,
+  },
+  {
+    path: "/terms-of-event",
+    element: <TermsOfEvent />,
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
